1c0c7816aeb7be071bc62c6322d88f934f45c1eb
msirazu <thebdrazu@gmail.com>
2026-02-02 18:55:47 +0600

    our medicine hub section working

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import HeroBanner from "../../components/header/HeroBanner";
 
@@ -15,7 +15,27 @@ const Home = () => {
     console.log(products);
     return (
         <Box>
+            <Box component='section'>
             <HeroBanner/>
+            </Box>
+            <Box component='section' sx={{mx: 10, my: 5}}>
+                <Box sx={{textAlign: 'center'}}>
+            <Typography sx={{backgroundColor: '#ECFDF5', display: 'inline-block', px: 2, py: 1, borderRadius: '25px', color: '#007A55', fontWeight: 600}} variant="p" component='p'>
+                Trusted Ayurvedic Remedies
+            </Typography>
+            <Typography sx={{my: 3, fontWeight: 700}} variant="h3" component='h2'>
+                Our Medicine Hub
+            </Typography>
+            <Typography sx={{maxWidth: '650px', textAlign: 'center', mx: 'auto'}} variant="p" component='p'>
+                Discover physician-approved natural formulas tailored for modern Bangladeshi lifestyles. Every product is vetted for purity, safety, and fast relief so you can shop with complete confidence.
+            </Typography>
+                </Box>
+            </Box>
+
+            <Box component='section'>
+
+            </Box>
+
         </Box>
     );
 };
