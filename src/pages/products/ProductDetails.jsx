7390bbd01e4e212eac219abac84209c8155ca36c
msirazu <thebdrazu@gmail.com>
2026-02-04 11:32:39 +0600

    product card info added

--- src/pages/products/ProductDetails.jsx
+++ src/pages/products/ProductDetails.jsx
@@ -19,19 +19,32 @@ const ProductDetails = () => {
     return <p>Product not found</p>;
     }
 
-    const { name, price, imageUrl, quantity, quantityType, brand, category, subCategory, id } = singleProduct;
+    const { name, price, imageUrl, quantity, quantityType, brand, category, subCategory, id, sku, stock } = singleProduct;
 
     return (
         <>
         <Box sx={{display: 'flex', justifyContent: 'center'}}>
         <Container sx={{maxWidth: '1400px', display: 'grid', gridTemplateColumns: {md: 'repeat(3, 1fr)', xs: 'repeat(1, 1fr)'}, gap: 3, mx: {md: 0, xs: 0}, my: {md: 2, xs: 2}}}>
-            <Box sx={{gridColumn: {md: 'span 1', xs: 'span 3'}, border: '1px solid #dddddd', borderRadius: 2}}>
-                <Box sx={{width: '100%'}} component='img' src={imageUrl}/>
+            <Box sx={{gridColumn: {md: 'span 1', xs: 'span 3'}, border: '1px solid #dddddd', borderRadius: 2, display: 'flex', justifyContent: 'center'}}>
+                <Box sx={{maxWidth: '400px'}} component='img' src={imageUrl}/>
             </Box>
-            <Box sx={{gridColumn: {md: 'span 2', xs: 'span 3'}, border: '1px solid #dddddd', p: 2, borderRadius: 2}}>
+            <Box sx={{gridColumn: {md: 'span 2', xs: 'span 3'}, border: '1px solid #dddddd', p: 5, borderRadius: 2}}>
+                <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: {xs: 'column', sm: 'row'}, gap: 3}}>
+                <Box>
                 <Typography sx={{fontWeight: 700, textTransform: 'capitalize'}} component='h1' variant="h4">
                     {name}
                 </Typography>
+                <Typography sx={{fontSize: '14px'}} component='p' variant="p">
+                    {`SKU: ${sku}`}
+                    <Typography sx={{ml: 2}} variant="p" component='span'>
+                    {`Stock: ${stock}`}
+                    </Typography>
+                </Typography>
+                </Box>
+                <Typography sx={{fontWeight: '700'}} component='h5' variant="h4">
+                    {`৳ ${price}`}
+                </Typography>
+                </Box>
             </Box>
         </Container>
         </Box>
--- src/pages/products/ProductsHome.jsx
+++ src/pages/products/ProductsHome.jsx
@@ -10,13 +10,19 @@ const ProductsHome = ({ product }) => {
     return (
         <Box sx={{border: '1px solid #dddddd', boxShadow: 1, borderRadius: 2, p: 2}} component='section'>
             <Box sx={{textDecoration: 'none'}} to={`/product-details/${id}`} component={Link}>
-                <Box sx={{width: '100%'}} component='img' src={imageUrl}/>
-                <Typography sx={{fontSize: '20px', fontWeight: 500, textTransform: 'capitalize', mb: 1}} variant="p" component='h2'>
-                    {name}
+                <Box sx={{width: '100%', '&:hover': {backgroundColor: '#dddddd50'}}} component='img' src={imageUrl}/>
+                <Typography sx={{textTransform: 'capitalize', mb: 1}} component='h6' variant='p'>
+                    {`${subCategory} > ${brand} > ${category}`}
+                </Typography>
+                <Typography sx={{fontSize: '15px', fontWeight: 500, textTransform: 'capitalize', mb: 1, color: '#000000'}} variant="p" component='h2'>
+                    {`${name} - ${quantity}`}
+                    <Typography sx={{textTransform: 'uppercase', ml: 1}} variant='p' component='span'>
+                        {`${quantityType.slice(0, 3)}`}
+                    </Typography>
                 </Typography>
                 </Box>
                 <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                <Button onClick={() => handleAddToCart(id)} startIcon={<ShoppingCartIcon/>} variant="contained" sx={{fontSize: {xs: '12px', md: '15px'}, textTransform: 'capitalize'}}>Add to Cart</Button>
+                <Button onClick={() => handleAddToCart(id)} startIcon={<ShoppingCartIcon/>} variant="contained" sx={{fontSize: {xs: '12px', md: '15px'}, textTransform: 'capitalize', '& .MuiButton-startIcon': {marginRight: '4px'}, '& .MuiSvgIcon-root': {fontSize: {xs: '15px', md: '20px'}}, width: '100%'}}>Add to Cart</Button>
                 </Box>
         </Box>
     );
